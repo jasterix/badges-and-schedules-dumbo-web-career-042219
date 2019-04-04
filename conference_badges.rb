fa01672e8fd8a6feb63d10e90d410da2e52e6171
Jasterix C <loune.calixte@gmail.com>
2019-04-04 20:53:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,13 +10,6 @@ def batch_badge_creator(attendees)
   arr
   end
 
-# def batch_badge_creator(names)
-#   names.each do |name|
-#     badge_maker(name)
-#     #puts "Hello, my name is" #{name}
-#   end
-# end
-
 def assign_rooms(names)
   arr=[]
   names.each_with_index {|value, index| arr << "Hello, #{value}! You'll be assigned to room #{index+1}!"}
