658952d226b5c019730c274b7f450e91e412e6cf
Jasterix C <loune.calixte@gmail.com>
2019-04-04 21:34:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,8 +17,9 @@ def assign_rooms(names)
 end
 
 def printer(names)
-  names.each do
-  puts "#{batch_badge_creator(names)}"
+  batch_badge_creator(names).each do |name|
+    puts name
+    puts assign_rooms(names)
 end
 end
   
